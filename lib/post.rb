0261bc91560aac000ce9050fdca26b48819d9199
Mike Cooper <mikescooper@icloud.com>
2019-09-09 01:56:49 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,6 +1,16 @@
 class Post 
   
-  attr_accessor :name, :author 
+  attr_accessor :title, :author 
   
+  @@all = []
+  
+  def initialize(title)
+    @title = title 
+    @@all << self
+  end 
+  
+  def self.all 
+    @@all
+  end 
   
 end 
