4b9d111e16b3d4e8fd4c44fa81d722c1574dc204
Mike Cooper <mikescooper@icloud.com>
2019-09-09 01:33:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,13 +9,10 @@ class Artist
     @songs = []
   end 
   
-  def songs 
-    self.song.all 
-  end 
-  
   def add_song(song)
       @songs << song 
       song.artist = self 
+      binding.pry
   end  
   
 end
