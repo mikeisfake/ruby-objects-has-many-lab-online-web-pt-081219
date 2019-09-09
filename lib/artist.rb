26e36e8821c9f862c286ebf2e0cf2e8a0e69d4e2
Mike Cooper <mikescooper@icloud.com>
2019-09-09 01:44:01 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -9,13 +9,17 @@ class Artist
     @songs = []
   end 
   
-  def add_song(song)
-      @songs << song 
-      song.artist = self 
-  end  
-  
   def songs
     Song.all.select {|song| song.artist == self}
   end
+  
+  def add_song(song)
+      @songs << song 
+      song.artist = self 
+  end 
+  
+  def add_song_by_name(song)
+    Song.new 
+  end 
   
 end
