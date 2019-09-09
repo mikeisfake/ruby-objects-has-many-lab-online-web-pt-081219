230e7c84e0e7bfcfa084b44bea8db279a67663be
Mike Cooper <mikescooper@icloud.com>
2019-09-09 01:37:59 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -2,11 +2,11 @@ require 'pry'
 
 class Artist 
   
-  attr_accessor :name
+  attr_accessor :name, :song
   
   def initialize(name)
     @name = name
-    @songs = song.all 
+    @songs = []
   end 
   
   def add_song(song)
